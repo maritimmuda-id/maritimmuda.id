d640145fce2fb9efeb37cb737e5446417c8ee846
myogaazwar <yogaazwar123@gmail.com>
2023-10-09 15:41:26 +0700

    change database and config for localhost

--- application/config/config.php
+++ application/config/config.php
@@ -23,16 +23,16 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$base_url .= "://" . @$_SERVER['HTTP_HOST'];
-$base_url .=     str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $base_url;
-//$config['base_url'] = 'http://localhost/maritimmuda.id';
-// // $config['base_url'] = 'http://www.maritimmuda.id';
+// $base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+// $base_url .= "://" . @$_SERVER['HTTP_HOST'];
+// $base_url .=     str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+// $config['base_url'] = $base_url;
+$config['base_url'] = 'http://localhost/maritimmuda.id';
+// // // $config['base_url'] = 'http://www.maritimmuda.id';
 
-$base_url = (isset($_SERVER['server_site']) ? $_SERVER['server_site'] : "http://localhost/maritimmuda-home");
-// // $base_url = (isset($_SERVER['server_site']) ? $_SERVER['server_site'] : "http://localhost:8081/maritim/maritimmuda-home/");
-$config['base_url'] = $base_url;
+// $base_url = (isset($_SERVER['server_site']) ? $_SERVER['server_site'] : "http://localhost/maritimmuda-home");
+// // // $base_url = (isset($_SERVER['server_site']) ? $_SERVER['server_site'] : "http://localhost:8081/maritim/maritimmuda-home/");
+// $config['base_url'] = $base_url;
 
 // $base_url = (isset($_SERVER['server_site']) ? $_SERVER['server_site'] : "http://localhost/maritim/maritimmuda-home/");
 // $config['base_url'] = $base_url;
