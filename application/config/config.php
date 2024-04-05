f8cb1b3b7eae74acad1e0cccb0e5072f590bc13c
dzkyydn <monk198monk@gmail.com>
2024-04-05 10:27:10 +0700

    update: fix database and config last update

--- application/config/config.php
+++ application/config/config.php
@@ -25,15 +25,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 
 //FOR ONLINE PURPOSES
-// $base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-// $base_url .= "://" . @$_SERVER['HTTP_HOST'];
-// $base_url .=     str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-// $config['base_url'] = $base_url;
-// $base_url = (isset($_SERVER['server_site']) ? $_SERVER['server_site'] : "http://localhost/maritimmuda-home");
-// $config['base_url'] = $base_url;
+$base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$base_url .= "://" . @$_SERVER['HTTP_HOST'];
+$base_url .=     str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $base_url;
+$base_url = (isset($_SERVER['server_site']) ? $_SERVER['server_site'] : "http://localhost/maritimmuda-home");
+$config['base_url'] = $base_url;
 
 // FOR LOCAL DEVELOPMENT ONLY
-$config['base_url'] = 'http://localhost/maritimmuda.id';
+// $config['base_url'] = 'http://localhost/maritimmuda.id';
 
 //MISC CONFIGURATIONS
 // $config['base_url'] = 'http://www.maritimmuda.id';
