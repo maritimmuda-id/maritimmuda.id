b08a6db3ba1c42eb85a28e8b8f5d75bae8c7708a
dzkyydn <m.dzakiyyudin@gmail.com>
2024-06-12 15:59:14 +0700

    update: inamarys page

--- application/views/arungisamudra/home_inamarys.php
+++ application/views/arungisamudra/home_inamarys.php
@@ -133,6 +133,178 @@
         </div>
         <!-- end section -->
         <!-- start section -->
+        <div style="background-color: #0071e7; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-ori.png');">
+            <section style="padding-bottom: 0;">
+                <div class="container margin-one-bottom">
+                    <div class="row">
+                        <div class="col-lg-8 col-md-8 col-sm-10 col-xs-12 text-center center-col margin-eight-bottom xs-margin-30px-bottom last-paragraph-no-margin">
+                            <h4 class="text-center harond-1" style="color: white;">Rundown Kegiatan</h4>
+                            <br>
+                            <br>
+                            <!-- Row 1 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png'); background-position: right;">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">1</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Persiapan</b></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 2 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png'); background-position: right;">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">2</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Kunjungan ke Sentra Pemberdayaan Pemuda Bidang Kemaritiman Nasional (Prestasi Hub Maritim Muda)</b></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 3 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png');">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">3</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Pembukaan MC, Pembacaan doa, dan menyanyikan lagu Indonesia Raya</b></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 4 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png'); background-position: right;">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">4</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Sambutan oleh Ketua Umum Maritim Muda Nusantara, Ketua STIE Indonesia Jakarta, dan Kepala LLDikti Wilayah III</b></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 5 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png');">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">5</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Pembukaan Resmi Acara Bersama Narasumber Sambutan & Dokumentasi</b></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 6 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png'); background-position: right;">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">6</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Special Opening Performance</b></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 7 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png');">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">7</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Grand Launching:</b>
+                                            <br>1. Maritim Muda Diving Club
+                                            <br>2. Digibluecamp: Learning Management System for Blue Economy Studies and Business Development
+                                            <br>3. All New Maritim Muda Hub V.3
+                                            <br>4. Website Indonesian Youth Economic Leaders (IYEL)
+                                            <br>5. Maritime Tecnopreneurs Incubator (MARITECHTOR)
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 8 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png'); background-position: right;">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">8</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Penandatanganan MoU & Dokumentasi</b></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 9 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png');">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">9</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Talkshow Session 1: Blue Economy & Education</b>
+                                            <br>1. Dr. I Nyoman Radiarta, Kepala BPPSDM KP KKP dan Penasihat Maritim Muda Nusantara
+                                            <br>2. Ibu Martini Mohamad Paham, Deputi Bidang Sumber Daya dan Kelembagaan, Kemenparekraf/Baparekraf
+                                            <br>3. Bapak Drs. H. Andri Yansyah, M.H., Kepala Dinas Pemuda dan Olahraga DKI Jakarta
+                                            <br>4. Ricky Soeraputra, Co-Founder Kristal Klear Dive Group
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 10 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png');">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">10</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Talkshow Session 2: Blue Economy & Marine Conservation</b>
+                                            <br>1. Kevin Juan J, Advisor CILT Next Generation
+                                            <br>2. Alexandra Maheswari Waskita, Marine Sustainable Financing Specialist, WCS Indonesia
+                                            <br>3. Erwin Falufi Irianti, Koordinator Nasional Penjaga Laut
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Row 11 -->
+                            <div class="row margin-four-bottom xs-margin-10px-bottom">
+                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <div style="border-radius: 15px 15px 0 0; background-color: #f84535; background-image:url('<?php echo site_url('assets/') ?>images/arungisamudra/asset/arungisamudra_bg-patern-4.png');">
+                                        <h6 style="color: white; margin: 0; padding: 10px 0;"><span class="harond-1">11</span></h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+                                    <div class="card" style="border-radius: 0 0 20px 20px; background-color: #f7f0d3; border: 5px solid #f7f0d3; border-color: #f7f0d3;">
+                                        <p class="text-left xs-text-center" style="color: #030a95; padding: 20px;"><b>Closing</b></p>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- end section -->
+        <!-- start section -->
         <section class="wow fadeIn animated color-code md-no-background-img" id="section-down" data-color="8" style="padding-bottom: 0">
             <div class="container margin-one-bottom">
                 <div class="row">
